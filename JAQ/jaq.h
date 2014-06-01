b8adc05329d1518878702907fa59d1c57733c02d
martin-lim <martin.lim.au@gmail.com>
2014-06-02 00:33:18 +1000

    cosmetic updates

--- JAQ/jaq.h
+++ JAQ/jaq.h
@@ -2,7 +2,9 @@
  * jaq.h
  *
  * Created: 16/02/2012 1:20:46 AM
- *  Author: Shavi
+ * Last Modified : 02/06/2014
+ *  Author: Shavi Nachman
+ *  Modified: Martin Lim
  */ 
  #ifndef _JAQ_H_
 	#define _JAQ_H_
@@ -15,9 +17,11 @@
 
 	#define CYCLES_PER_US ((F_CPU+500000)/1000000)
 
-	//#include <avr/io.h>
+	//standard library
 	#include <avr/interrupt.h>
 	#include <stdint.h>
+
+	//custom library
 	#include "init_io.h"
 	#include "stabilityControl.h"
 	#include "motorControl.h"
