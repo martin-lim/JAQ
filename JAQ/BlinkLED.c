38005eb0a731ebdff0ee951c6eb4c3c462490291
shavinachman <shavi64@gmail.com>
2014-06-01 19:28:10 +1000

    PB5 -> PORTB5
    change PB5 to PORTB5
    Also remove extra <<

--- JAQ/BlinkLED.c
+++ JAQ/BlinkLED.c
@@ -12,16 +12,16 @@
 void inithardware() //TODO: move to init_IO
 {
 
-	DDRB |= (1<<PB5);
+	DDRB |= (1<<PORTB5);
 }
 
 int BlinkLED(int waittime)
 {
 	for (int i=0;i<500000;i++)
 	{
-		PORTB |= (1<<<<PB5);
+		PORTB |= (1<<PORTB5);
 		_delay_ms(waittime);
-		PORTB &= ~(1<<<<PB5);
+		PORTB &= ~(1<<PORTB5);
 		_delay_ms(waittime);
 		
 	}
