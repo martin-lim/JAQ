8f9570675775a2ee19b8efd1d937b4c0911efaaf
Martin Lim <martin.lim.au@gmail.com>
2014-06-10 18:23:08 +1000

    cosmetic change

--- JAQ/motorControl.h
+++ JAQ/motorControl.h
@@ -10,15 +10,11 @@
 	#define _MOTOR_CONTROL_H_
 	
 	void motorStartUpSequence (void);
-	
 	void motorStop();
 	
 	void _motor1(uint16_t freq);
-	
 	void _motor2(uint16_t freq);
-	
 	void _motor3(uint16_t freq);
-	
 	void _motor4(uint16_t freq);
 	
 #endif
